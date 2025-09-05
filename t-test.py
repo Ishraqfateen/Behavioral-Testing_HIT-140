d00101e1b240c9a9f2d3f5b04c18ed168cccc3d2
Fateen Ishraq Rahman <fateenishraq99@gmail.com>
2025-09-05 22:41:05 +0930

    Update t-test.py

--- t-test.py
+++ t-test.py
@@ -1,9 +1,1 @@
-import pandas as pd
-
-df = pd.read_csv("merged_grouped.csv")
-print(df.info())
-print(df["rat_present"].value_counts())
-print("======new_Stuff=====")
-print(df.describe())
-print(df['rat_present'].value_counts(normalize=True))
 
