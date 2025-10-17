4fb2402f02bfcecd41cdb414ce12998d69273ea0
Fateen Ishraq Rahman <fateenishraq99@gmail.com>
2025-10-18 04:47:32 +0930

    Update and rename moonlight.py to A3/moonlight.py

--- A3/moonlight.py
+++ A3/moonlight.py
@@ -10,7 +10,7 @@ df['time'] = pd.to_datetime(df['time'], errors='coerce', dayfirst=True)
 def illumination_from_phase(date):
     if pd.isna(date):
         return None
-    phase = moon.phase(date)  # 0–29.53 (days in lunar cycle)
+    phase = moon.phase(date) 
     illumination = (1 - math.cos(2 * math.pi * phase / 29.53)) / 2 * 100
     return round(illumination, 2)
 
@@ -19,4 +19,5 @@ df['moon_illumination'] = df['time'].apply(illumination_from_phase)
 
 df.to_csv("merged_with_moonlight.csv", index=False)
 print("New file saved as 'merged_with_moonlight.csv'")
+
 
